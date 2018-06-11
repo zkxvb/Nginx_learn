3392cbcaa82a537592c11395abf4d6fb80437d53
zkxvb <zkxvb@outlook.com>
2018-06-11 05:05:05 -0400

    edit by zkx

---
+++ module_source/ngx_http_echo_module/ngx_http_echo_module.c
@@ -0,0 +1,9 @@
+/*************************************************************************
+	> File Name: ngx_http_echo_module.c
+	> Author: zkxvb
+	> Mail:  zkxvb@outlook.com 
+	> Created Time: Mon 11 Jun 2018 05:04:46 AM EDT
+ ************************************************************************/
+
+#include<stdio.h>
+
